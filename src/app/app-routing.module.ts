1ce6730cfaa616b9066b8ba88d4a15a8735a1d56
harinvb <harinvb@gmail.com>
2021-04-20 15:07:40 +0530

    About Deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './Components/about/about.component';
 import { HomeComponent } from './Components/home/home.component';
 import { NoPageComponent } from './Components/no-page/no-page.component';
 
@@ -21,10 +20,6 @@ const routes: Routes = [
     component: HomeComponent,
   },
   {
-    path: 'about',
-    component: AboutComponent,
-  },
-  {
     path: '**',
     component: NoPageComponent,
   },
@@ -38,7 +33,6 @@ export class AppRoutingModule {}
 export const routingComponents = [
   LoginComponent,
   AppComponent,
-  AboutComponent,
   HomeComponent,
   NoPageComponent,
 ];
